ea2d20b603f3a370cbeceef1e1f6a95013581294
Weijuan Chen <68696117+weijuan82113@users.noreply.github.com>
2024-08-24 07:59:50 +0000

    UPDATE ex02 modify main.cpp test content

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -2,7 +2,8 @@
 #include <cstdlib>
 #include "Array.h"
 
-#define MAX_VAL 2
+#define MAX_VAL 750
+#define LENGTH 3
 int main(int, char**)
 {
     Array<int> numbers(MAX_VAL);
@@ -47,13 +48,24 @@ int main(int, char**)
         std::cerr << e.what() << '\n';
     }
 
-    for (int i = 0; i < MAX_VAL; i++)
+    for (int i = 0; i < LENGTH; i++)
     {
         //non-const operator[]
         numbers[i] = rand();
         //const operator[]
         std::cout << "numbers["<< i <<"]" << numbers[i] << std::endl;
     }
+
+    //--------copy test--------
+    Array<int> arr_cpy_constructor(numbers);
+    Array<int> arr_cpy_copy_assingment = numbers;
+    for (int i = 0; i < LENGTH; i++)
+    {
+        std::cout << "numbers[" << i << "]: " << numbers[i] << std::endl;
+        std::cout << "arr_cpy[" << i << "]: " << arr_cpy_constructor[i] << std::endl;
+        std::cout << "arr_assign[" << i << "]: " << arr_cpy_copy_assingment[i] << std::endl;
+    }
+    //--------copy test--------
     delete [] mirror;//
     return 0;
 }
