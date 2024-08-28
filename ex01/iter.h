a2b200f805c8957add076ce21a473d949ee0188d
Weijuan Chen <68696117+weijuan82113@users.noreply.github.com>
2024-08-28 01:50:07 +0000

    UPDATE ex01 change template expression to static

--- ex01/iter.h
+++ ex01/iter.h
@@ -6,20 +6,20 @@
 //インスタンス化して、テンプレート関数で実行するテンプレート関数
 //値を変更しない時に使う関数
 template <typename T>
-void printValue(const T& value) {
+static void printValue(const T& value) {
 	std::cout << "Const Value: " << value << std::endl;
 }
 
 //インスタンス化して、テンプレート関数で実行するテンプレート関数
 //値を変換したい時に使う関数
 template <typename T>
-void printValue(T& value) {
+static void printValue(T& value) {
 	std::cout << "Value: " << value << std::endl;
 }
 
 //const テンプレート関数
 template <typename T>
-void iter(const T* array, int length, void (*func)(const T& value))
+static void iter(const T* array, int length, void (*func)(const T& value))
 {
 	for(int i = 0; i < length ; i ++)
 		func(array[i]);
@@ -27,7 +27,7 @@ void iter(const T* array, int length, void (*func)(const T& value))
 
 //テンプレート関数
 template <typename T>
-void iter(T* array, int length, void (*func)(T& value))
+static void iter(T* array, int length, void (*func)(T& value))
 {
 	for(int i = 0; i < length ; i ++)
 		func(array[i]);
